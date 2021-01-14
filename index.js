19c0fe112865051ac1a33dbdf9800b6afb9a8c9e
maxivgg <maxivgg@gmail.com>
2021-01-14 12:46:58 -0300

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
-//app.use(express.static('public/build'));
+app.use(express.static('public/build'));
 
 db.mongoose
   .connect(db.url, {
